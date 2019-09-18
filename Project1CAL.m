a0a4c2ecd7cd74b791d6ff8bcd16fcb96f2be3cc
swag31415 <kidswagatam18@gmail.com>
2019-09-18 11:42:09 -0600

    Completed Question 3

--- Project1CAL.m
+++ Project1CAL.m
@@ -1,23 +1,44 @@
 % Project 1 for Calc 3
 % Authors are Swag, Kiro, and Manuel
 
+% Deciding on a step size
+h = (10 / 601);
+
 % Defining points
-t=0:10/601:10;
+domain = 0:h:10;
 
 % Defining curve that the spacethingy takes
-x_t=10.*sin(t)+5.*sin(5.*t)+2.5.*sin(2.3.*t);
-y_t=10.*cos(t)+5.*cos(5.*t)+2.5.*cos(2.3.*t);
-z_t=(0.001.*(t.^4)).*(1+(cos(2*pi.*t)).^2);
+x_t = @(t) 10.*sin(t)+5.*sin(5.*t)+2.5.*sin(2.3.*t);
+y_t = @(t) 10.*cos(t)+5.*cos(5.*t)+2.5.*cos(2.3.*t);
+z_t = @(t) (0.001.*(t.^4)).*(1+(cos(2*pi.*t)).^2);
 
-% Defining the plot
-r = plot3(x_t, y_t, z_t, 'b', 'LineWidth', 2);
+% Computing points along that curve
+x_set = x_t(domain);
+y_set = y_t(domain);
+z_set = z_t(domain);
+
+% Showing the points as a line
+plot3(x_set, y_set, z_set, 'b', 'LineWidth', 2);
 hold on; % Ensuring the 3d line stays and the markers below fall on said line
-plot3(x_t, y_t, z_t, 'o', 'MarkerFaceColor', 'green', 'MarkerEdgeColor', 'green', 'MarkerIndices', 1); % First Point
-plot3(x_t, y_t, z_t, 'o', 'MarkerFaceColor', 'red', 'MarkerEdgeColor', 'red', 'MarkerIndices', 602); % Last Point
-hold off; % Done adding points
+plot3(x_set, y_set, z_set, 'o', 'MarkerFaceColor', 'green', 'MarkerEdgeColor', 'green', 'MarkerIndices', 1); % First Point
+plot3(x_set, y_set, z_set, 'o', 'MarkerFaceColor', 'red', 'MarkerEdgeColor', 'red', 'MarkerIndices', 602); % Last Point
+hold off; % Done adding markers
 
 % Starting coords
-c_steal = [x_t(1), y_t(1), z_t(1)];
+c_steal = [x_t(0), y_t(0), z_t(0)];
 
 % Ending coords
-c_end = [x_t(602), y_t(602), z_t(602)];
+c_end = [x_t(10), y_t(10), z_t(10)];
+
+% Total displacement
+disp = sqrt(((c_steal(1) - c_end(1))^2) + ((c_steal(2) - c_end(2))^2) + ((c_steal(3) - c_end(3))^2))
+
+% Computing the inside of the arc-length integral
+d = sqrt(((diff(x_set).^2) + (diff(y_set).^2) + (diff(z_set).^2))./(h^2));
+
+% Computing the arclength integral using a Riemann Sum
+dist = 0;
+for i = 1:length(d)
+    dist = dist + (d(i) * h);
+end
+
